7b3d7648a757180ec633638c9ca94df963e86948
Viktor Hryhorchuk <viktor_hryhorchuk@epam.com>
2022-05-04 09:18:15 +0300

    Cleanup.

--- config/bot_main.rb
+++ config/bot_main.rb
@@ -10,43 +10,85 @@ def run_telegram_bot
     bot.listen do |message|
       case message
         when Telegram::Bot::Types::CallbackQuery
-          # Here you can handle your callbacks from inline buttons
-          if message.data == 'drinks'
-            drinks = Drink.all.to_a
-            res = []
-            drinks.each do |item|
-              res.push("Напій #{item.name}, Ціна #{item.price}")
+          # Here you can handle your callbacks from inline buttons   
+        if message.data.start_with?('end_test')
+          final_score = message.data[/(?<=score )\d+/, 0].to_i
+          results = Result.all.to_a.sort_by { |item| item[:score].to_i}
+          res_1 = results[0]
+          res_2 = results[1]
+          res_3 = results[2]
+
+          final_res = nil
+          if final_score > res_1["score"]
+            if final_score >= res_2["score"]
+              if final_score >= res_3["score"]
+                final_res = res_3["text"]
+              else
+                final_res = res_2["text"]
+              end
+            else
+              final_res = res_1["text"]
             end
-            kb = [
-              Telegram::Bot::Types::InlineKeyboardButton.new(text: 'Замовити напій', callback_data: 'order_drink'),
-              Telegram::Bot::Types::InlineKeyboardButton.new(text: 'Замовити страву', callback_data: 'order_meal'),
-            ]
-            markup = Telegram::Bot::Types::InlineKeyboardMarkup.new(inline_keyboard: kb)
-            bot.api.send_message(chat_id: message.from.id, text: "#{res.join("\n")}", reply_markup: markup)
-          elsif message.data == 'start_test'
-            # meals = Meal.all.to_a
-            # res = []
-            # meals.each do |item|
-              # res.push("Страва #{item.name}, Ціна #{item.price}")
-            # end
-            kb = [
-              Telegram::Bot::Types::InlineKeyboardButton.new(text: 'Замовити напій', callback_data: 'order_drink'),
-              Telegram::Bot::Types::InlineKeyboardButton.new(text: 'Замовити страву', callback_data: 'order_meal'),
-            ]
-            markup = Telegram::Bot::Types::InlineKeyboardMarkup.new(inline_keyboard: kb)
-            bot.api.send_message(chat_id: message.from.id, text: "#{res.join("\n")}", reply_markup: markup)
           end
+          
+          bot.api.send_message(chat_id: message.from.id, text: "Ваш підсумковий бал #{final_score}")
+          bot.api.send_message(chat_id: message.from.id, text: "#{final_res}")
+        
+        elsif message.data.start_with?('start_test')
+            questions = Question.all.to_a
+            q_results = []
+            current_question = message.data[/(?<=start_test )\d+/, 0].to_i
+            score = message.data[/(?<=score )\d+/, 0].to_i
+
+            if current_question >= questions.length()
+              kb = [
+                Telegram::Bot::Types::InlineKeyboardButton.new(text: "Отримати результат", callback_data: "end_test score #{score}"),
+              ]
+              markup = Telegram::Bot::Types::InlineKeyboardMarkup.new(inline_keyboard: kb)
+              bot.api.send_message(chat_id: message.from.id, text: "Тест завершено", reply_markup: markup)
+            else
+              question_ = questions[current_question]
+              q_to_a = QuestionToAnswer.where(question_id: question_.id)
+              answrs = []
+              q_to_a.to_a.each do |ans|
+                res = Answer.find(ans.answer_id)
+                answrs.push({
+                  "text": res.text,
+                  "score": res.score
+                })
+              end
+              question = {
+                question: question_.text,
+                answers: answrs
+              }
+              answers = question[:answers]
+              answ_1 = answers[0]
+              answ_2 = answers[1]
+              answ_3 = answers[2]
+              score_1 = answ_1[:score] + score
+              score_2 = answ_2[:score] + score
+              score_3 = answ_3[:score] + score
+              current_question += 1
+  
+              kb = [
+                Telegram::Bot::Types::InlineKeyboardButton.new(text: "#{answ_1[:text]}", callback_data: "start_test #{current_question} score #{score_1}"),
+                Telegram::Bot::Types::InlineKeyboardButton.new(text: "#{answ_2[:text]}", callback_data: "start_test #{current_question} score #{score_2}"),
+                Telegram::Bot::Types::InlineKeyboardButton.new(text: "#{answ_3[:text]}", callback_data: "start_test #{current_question} score #{score_3}"),
+              ]
+              markup = Telegram::Bot::Types::InlineKeyboardMarkup.new(inline_keyboard: kb)
+              bot.api.send_message(chat_id: message.from.id, text: "#{question[:question]}", reply_markup: markup)  
+            end
+           end
         when Telegram::Bot::Types::Message
           case message.text
             when '/start'
               kb = [
-                Telegram::Bot::Types::InlineKeyboardButton.new(text: 'Почати тест!', callback_data: 'start_test'),
+                Telegram::Bot::Types::InlineKeyboardButton.new(text: 'Почати тест!', callback_data: 'start_test 0 score 0'),
               ]
               markup = Telegram::Bot::Types::InlineKeyboardMarkup.new(inline_keyboard: kb)
               bot.api.send_message(chat_id: message.chat.id, text: "Ласкаво просимо, #{message.from.first_name}. Дізнайся наскільки твоє харчування здорове!", reply_markup: markup)
             when '/stop'
-              order.clear
-                bot.api.send_message(chat_id: message.chat.id, text: "Дякуємо що були з нами, #{message.from.first_name}!")
+              bot.api.send_message(chat_id: message.chat.id, text: "Дякуємо що були з нами, #{message.from.first_name}!")
             else
               bot.api.send_message(chat_id: message.chat.id, text: "Вибачте, команда не підтримується, виберіть:
                 /start
