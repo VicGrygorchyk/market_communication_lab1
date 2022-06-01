6a7c8bb683b4ea6ab9caca6b1b431de4002b84f8
Viktor Hryhorchuk <viktor_hryhorchuk@epam.com>
2022-06-01 09:00:27 +0300

    Temp save.

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,6 @@ module TelegramBot
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    Thread.new { run_telegram_bot }
+    # Thread.new { run_telegram_bot }
   end
 end
